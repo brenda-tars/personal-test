f25cb12d7b703f40ae6001355895f6f4872c9385
brenda-tars <xuqing@tars-ai.com>
2026-07-20 14:27:47 +0800

    1

--- extract.sh
+++ extract.sh
@@ -1,7 +1,7 @@
 cat script.sh > new_install.run
 echo "__APOLLO_INSTALL_PAYLOAD_BELOW__" >> new_install.run
 cat payload.tar >> new_install.run
-
+tar -cf - -C payload . >> new_install.run
 
 awk '/^__APOLLO_INSTALL_PAYLOAD_BELOW__$/ {exit} {print}' install.run > script.sh
 
